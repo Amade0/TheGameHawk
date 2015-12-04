5cbbe62e9227425cf5183ab0d33b022ca32b51ef
Amade <ynimos@gmail.com>
2015-12-04 15:37:24 -0600

    adding part of login functionality and logout script

--- login.php
+++ login.php
@@ -3,9 +3,51 @@
 	<title>Log in</title>
 	<link rel="stylesheet" type="text/css" href="stylesheet.css">
 </head>
+
+<?php
+session_start();
+include_once 'database.php';
+
+if(isset($_SESSION['user']) != "")
+{
+	header("Location: index.php");
+}
+if(isset($_POST['btn-login']))
+{
+	$loginName = mysql_real_escape_string($_POST['loginName']);
+	$upass = md5($_POST['password']);
+	$res = mysql_query("SELECT * FROM users WHERE loginName='$loginName'");
+	$row = mysql_fetch_array($res);
+	if($row['passwordHash'] == $upass)
+	{
+		$_SESSION['user'] = $row['user_id'];
+		header("Location: index.php");
+	}
+	else
+	{
+		?>
+		<script>alert('Invalid username or password. Please try again.');</script>
+		<?php
+	}
+}
+?>
+
 <body>
-	<?php
-		echo "php working!<br />";
-	?>
+	<div id="login-form">
+		<form method="post">
+			<table border="0">
+				<tr>
+					<td>Login name:</td><td><input type="text" name="loginName" placeholder="Login name" required /></td>
+				</tr>
+				<tr>
+					<td>Password:</td><td><input type="password" name="password" placeholder="••••••••" required /></td>
+				</tr>
+				<tr>
+					<td colspan=2 style="text-align:center"><button type="submit" name="btn-login">Log in</button></td>
+				</tr>
+			</table>
+		</form>
+	</div>
+	Don't have an account? <a href="./register.php"> Register here!</a><br />
 </body>
 <html>
---
+++ logout.php
@@ -0,0 +1,20 @@
+<!-- adapted from http://www.codingcage.com/2015/01/user-registration-and-login-script-using-php-mysql.html -->
+<?php
+session_start();
+
+if(!isset($_SESSION['user']))
+{
+	header("Location: index.php");
+}
+else if(isset($_SESSION['user'])!="")
+{
+	header("Location: home.php");
+}
+
+if(isset($_GET['logout']))
+{
+	session_destroy();
+	unset($_SESSION['user']);
+	header("Location: index.php");
+}
+?>
