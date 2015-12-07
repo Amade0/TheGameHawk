4c43c7fb125d8a3a930d7e269cd3f0b8addbe0f9
croemer <coltonroemer@yahoo.com>
2015-12-06 23:28:18 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,15 +1,12 @@
 <html>
 <head>
-	<title>The Game Hawk</title>
+	<title><big>The Game Hawk</big></title>
 	<link rel="stylesheet" type="text/css" href="stylesheet.css">
 </head>
 <body>
-	<?php
-		echo 'php working!<br />';
-	?>
 	<ul>
-		<li><a href="./register.php">Register</a></li>
-		<li><a href="./login.php">Log in</a></li>
+		<p align="center"><li><a href="./register.php">Register</a></li></p>
+		<p align="center"><li><a href="./login.php">Log in</a></li></p>
 	</ul>
 </body>
 <html>
