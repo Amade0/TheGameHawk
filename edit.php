8ad8f0c40c9df016916736527991c086af02d523
Amade <ynimos@gmail.com>
2015-12-08 21:31:10 -0600

    major editing improvements
    now updates all fields at once with a single button, code readability
    greatly improved

--- edit.php
+++ edit.php
@@ -9,34 +9,14 @@ if(isset($_SESSION['user']) == "")
 $user = $_SESSION['user'];
 include_once 'database.php';
 
-if(isset($_POST['btn-change-email']))
+if(isset($_POST['btn-save']))
 {
 	$emailAddress = mysql_real_escape_string($_POST['emailAddress']);
-	mysql_query("UPDATE `users` SET `emailAddress` = '$emailAddress' WHERE `users`.`userID` = $user;");
-}
-
-if(isset($_POST['btn-change-password']))
-{
 	$passwordHash = md5($_POST['password']);
-	mysql_query("UPDATE `users` SET `passwordHash` = '$passwordHash' WHERE `users`.`userID` = $user;");
-}
-
-if(isset($_POST['btn-change-display-name']))
-{
 	$displayName = mysql_real_escape_string($_POST['displayName']);
-	mysql_query("UPDATE `users` SET `displayName` = '$displayName' WHERE `users`.`userID` = $user;");
-}
-
-if(isset($_POST['btn-change-location']))
-{
 	$location = mysql_real_escape_string($_POST['location']);
-	mysql_query("UPDATE `users` SET `location` = '$location' WHERE `users`.`userID` = $user;");
-}
-
-if(isset($_POST['btn-change-game-list']))
-{
 	$gameList = mysql_real_escape_string($_POST['gameList']);
-	mysql_query("UPDATE `users` SET `gameList` = '$gameList' WHERE `users`.`userID` = $user;");
+	mysql_query("UPDATE `users` SET `emailAddress` = '$emailAddress', `passwordHash` = '$passwordHash', `displayName` = '$displayName', `location` = '$location', `gameList` = '$gameList' WHERE `users`.`userID` = $user;");
 }
 
 ?>
@@ -49,45 +29,69 @@ if(isset($_POST['btn-change-game-list']))
 
 <body>
 
-<?php
-echo('<div id="edit-form">
+
+<div id="edit-form">
 	<form method="post">
 		<table border="0">
 			<tr>
-				<td>Login name:</td><td colspan=2>');
+				<td>Login name:</td>
+				<td>
+					<?php
 					//display login name
 					$res = mysql_query("SELECT * FROM users WHERE userID='$user'");
 					$row = mysql_fetch_array($res);
 					echo($row['loginName']);
-				echo('</td>
+					?>
+				</td>
 			</tr>
 			<tr>
-				<td>Email address:</td><td><input type="email" name="emailAddress" placeholder="');
-					echo($row['emailAddress']);
-				echo('" /></td><td><button type="submit" name="btn-change-email">Change</button></td>
+				<td>Email address:</td>
+				<td>
+					<?php
+					$emailAddress = $row['emailAddress'];
+					echo("<input type=\"email\" name=\"emailAddress\" value=\"$emailAddress\" />");
+					?>
+				</td>
 			</tr>
 			<tr>
-				<td>Password:</td><td><input type="password" name="password" placeholder="••••••••"/></td><td><button type="submit" name="btn-change-password">Change</button></td>
+				<td>Password:</td>
+				<td><input type="password" name="password" placeholder="••••••••"/></td>
 			</tr>
 			<tr>
-				<td>Display name:</td><td><input type="text" name="displayName" placeholder="');
-					echo($row['displayName']);
-				echo('" /></td><td><button type="submit" name="btn-change-display-name">Change</button></td>
+				<td>Display name:</td>
+				<td>
+					<?php
+					$displayName = $row['displayName'];
+					echo("<input type=\"text\" name=\"displayName\" value=\"$displayName\" />");
+					?>
+				</td>
 			</tr>
 			<tr>
-				<td>Location:</td><td><input type="text" name="location" placeholder="');
-					echo($row['location']);
-			echo('" /></td><td><button type="submit" name="btn-change-location">Change</button></td>
+				<td>Location:</td>
+				<td>
+					<?php
+					$location = $row['location'];
+					echo("<input type=\"text\" name=\"location\" value=\"$location\" />");
+					?>
+				</td>
 			</tr>
 			<tr>
-				<td>Games:</td><td><input type="text" name="gameList" placeholder="');
-					echo($row['gameList']);
-				echo('" /></td><td><button type="submit" name="btn-change-game-list">Change</button></td>
+				<td>Games:</td>
+				<td>
+					<?php
+					$gameList = $row['gameList'];
+					echo("<input type=\"text\" name=\"gameList\" value=\"$gameList\" />");
+					?>
+				</td>
+			</tr>
+			<tr>
+				<td colspan=2>
+					<button type="submit" name="btn-save">Save changes</button>
+				</td>
 			</tr>
 		</table>
 	</form>
-</div>');
-?>
+</div>
 
 <a href="./home.php">Back to home</a>
 
